b7b2af7bb90e5ed6943ef2ba564608e4e6347578
WainZeng <zk0g0w@gmail.com>
2026-06-11 20:25:36 +0800

    chore(oauth2): 移除 consent mock 数据（后端已实现）

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,8 +1,7 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 
 import area from './area'
-import oauth2Consent from './oauth2-consent'
 
 export function setupProdMockServer() {
-  createProdMockServer([...area, ...oauth2Consent])
+  createProdMockServer([...area])
 }
