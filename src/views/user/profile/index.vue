8ee5d6e8fece11f8929f58e594dce698e13b4258
WainZeng <zk0g0w@gmail.com>
2026-06-13 14:40:16 +0800

    feat: 个人中心集成授权管理和会话管理 tab
    none

--- src/views/user/profile/index.vue
+++ src/views/user/profile/index.vue
@@ -5,12 +5,22 @@
         <LeftBox />
       </a-col>
       <a-col :xs="24" :sm="24" :md="14" :lg="14" :xl="17" :xxl="17">
-        <div>
-          <PasswordPolicy />
-        </div>
-        <div style="margin-top: 16px">
-          <RightBox />
-        </div>
+        <a-card bordered class="gradient-card">
+          <a-tabs default-active-key="security">
+            <a-tab-pane key="security" title="安全设置">
+              <PasswordPolicy />
+            </a-tab-pane>
+            <a-tab-pane key="social" title="第三方账号">
+              <RightBox />
+            </a-tab-pane>
+            <a-tab-pane key="grants" title="已授权应用">
+              <GrantsTab />
+            </a-tab-pane>
+            <a-tab-pane key="sessions" title="活跃会话">
+              <SessionsTab />
+            </a-tab-pane>
+          </a-tabs>
+        </a-card>
       </a-col>
     </a-row>
   </div>
@@ -20,6 +30,8 @@
 import LeftBox from './BasicInfo.vue'
 import RightBox from './Social.vue'
 import PasswordPolicy from './Security.vue'
+import GrantsTab from '@/views/system/user/center/GrantsTab.vue'
+import SessionsTab from '@/views/system/user/center/SessionsTab.vue'
 
 defineOptions({ name: 'UserProfile' })
 </script>
